9b2bf31a6be18610bd8707f1459b52e303126f7a
Benoît Richard <kolaru@hotmail.com>
2019-06-14 20:45:11 +0200

    Consider name "" to be None in register_game

--- ranking.py
+++ ranking.py
@@ -82,6 +82,12 @@ class Ranking:
         return self.player_to_rank.get(player, "[unkown]")
 
     async def register_game(self, game, save=True, update_ranking=True):
+        if game["winner"] == "":
+            game["winner"] = None
+        
+        if game["loser"] == "":
+            game["loser"] = None
+            
         if game["winner"] is None or game["loser"] is None:
             return None
             
