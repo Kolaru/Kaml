0960d29ff5b63dc5118a171d915e2631d6b8bbaa
Benoît Richard <kolaru@hotmail.com>
2019-08-06 22:55:15 +0200

    Add Duchu ranking

--- src/ranking/__init__.py
+++ src/ranking/__init__.py
@@ -1,4 +1,7 @@
 from .trueskill_ranking import TrueSkillRanking
 from .eel_ranking import EelRanking
+from .duchu_ranking import DuchuRanking
 
-ranking_types = dict(trueskill=TrueSkillRanking, eel=EelRanking)
+ranking_types = dict(trueskill=TrueSkillRanking,
+                     eel=EelRanking,
+                     duchu=DuchuRanking)
