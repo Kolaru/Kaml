74041d324940485dd222bba78b1f3d549d8197ac
Benoît Richard <kolaru@hotmail.com>
2020-04-20 01:18:14 +0200

    Document truekill ranking

--- src/ranking/ranking.py
+++ src/ranking/ranking.py
@@ -12,13 +12,13 @@ class AbstractRanking:
 
     Additional keyword parameters are ignored.
 
-    Parameters
-    ==========
+    Arguments
+    =========
     name: str
         The unique name of the ranking.
 
-    Keyword parameters
-    ==========
+    Keyword arguments
+    =================
     bot: Kamlbot
         The discord Kamlbot using the ranking. Used to access discord related
         information about the players.
@@ -27,6 +27,16 @@ class AbstractRanking:
     earliest_timestamp: Unix timestamp
         The timestamp before which games are registered in the ranking.
         TODO Implement this.
+    history_dataframe: pandas.DataFrame
+        The dataframe used to store the history of this ranking. The following
+        columns are required:
+            = timestamp
+            = winner_id
+            - winner_rank
+            - winner_score
+            = loser_id
+            - loser_rank
+            - loser_score
     leaderboard_line: template string
         The template used to display player informations in the leaderboard.
     leaderboard_msgs: list of dict
@@ -37,7 +47,7 @@ class AbstractRanking:
         being computed.
     oldest_timestamp: Unix timestamp
         The timestamp after which games are registered in the ranking.
-    ranking_dataframe: pandas.Dataframe
+    ranking_dataframe: pandas.DataFrame
         The dataframe used to represent the ranking internally. The following
         columns are required:
             - rank
@@ -47,14 +57,20 @@ class AbstractRanking:
     def __init__(self, name,
                  bot=None,
                  description="A ranking",
+                 earliest_timestamp=np.Inf,
+                 history_dataframe=None,
                  leaderboard_line=None,
                  leaderboard_msgs=None,
                  mingames=0,
                  oldest_timestamp=0,
                  ranking_dataframe=None,
                  **kwargs):
+        logger.info(f"Generic initialization of the ranking named {name}")
+
         self.bot = bot
         self.description = description
+        self.earliest_timestamp = earliest_timestamp
+        self.history = history_dataframe
         self.leaderboard_line = leaderboard_line
         self.leaderboard_msgs = leaderboard_msgs
         self.mingames = mingames
@@ -78,7 +94,57 @@ class AbstractRanking:
         """
         Add a new player to the ranking with the given player ID.
 
+        New player must be initialized with
+            rank = np.nan
+            n_games = 0
+
         Must be implemented by subclasses.
+
+        Arguments
+        =========
+        player_id: int
+            The ID of the player to be added.
+        """
+        raise NotImplementedError()
+
+    def apply_new_states(self, game_id, winner_state, loser_state):
+        """
+        Apply ranking specific change to the ranking and history dataframes.
+
+        As opposed to the `compute_player_states` function, this one is allowed
+        to have side effect and can rely on the fact entries for the game
+        resulting in the given states have been added to the dataframes.
+
+        Arguments
+        =========
+        game_id: int
+            ID of the game that resulting in the given states. Is used to index
+            the game in the history DataFrame.
+        winner_state: dict
+            State of the winner returned by the compute_player_states function.
+        loser_state: dict
+            State of the loser returned by the compute_player_states function.
+        """
+
+    def compute_player_states(self, winner_id, loser_id):
+        """
+        Compute the new states of the players involved in a game during which
+        the player with ID `winner_id` won against the player with ID
+        `loser_id`.
+
+        This function must not have any side effect, to make sure that
+        the generic `AbstractRanking.register_game` method works as expected.
+
+        Must return a dict with a field "score". This dict will later be passed
+        to the `AbstractRanking.apply_new_states` method in which additional
+        updates can be performed with side effects.
+
+        Must be implemented by subclasses.
+
+        Arguments
+        =========
+        winner_id: int
+        loser_id: int
         """
         raise NotImplementedError()
 
@@ -88,8 +154,115 @@ class AbstractRanking:
         `stop`.
 
         Must be implemented by subclasses.
+
+        Arguments
+        =========
+        start: int
+        stop: int
         """
         raise NotImplementedError()
+
+    def register_game(self, winner_id=0, loser_id=0, timestamp=0, **kwargs):
+        """
+        Register a game in the ranking.
+
+        Keywords arguments
+        ==================
+        winner_id: int
+        loser_id: int
+        timestamp: int
+
+        Additional keyword arguments are ignored.
+        """
+        # Ignore games that are outside the considered period
+        if not (self.oldest_timestamp <= timestamp < self.earliest_timestamp):
+            return None
+
+        # Add missing players
+        if winner_id not in self.ranking.index:
+            self.add_new_player(winner_id)
+
+        if loser_id not in self.ranking.index:
+            self.add_new_player(loser_id)
+
+        # Request the new player states from method implemented by the subclass
+        winner_state, loser_state = self.compute_player_states(
+            winner_id,
+            loser_id)
+
+        winner_score = winner_state["score"]
+        loser_score = loser_state["score"]
+
+        self.ranking.loc[winner_id, "n_games"] += 1
+        self.ranking.loc[loser_id, "n_games"] += 1
+
+        # When the old score is not None that means this player was already
+        # previously ranked. Thus we remove their score from the score list
+        # and when we insert it again it will be correctly ranked. 
+        winner_old_rank = self.ranking.loc[winner_id, "rank"]
+
+        if winner_old_rank is not None:
+            winner_old_score = self.ranking.loc[winner_id, "score"]
+            self.sorted_scores.remove(winner_old_score)
+
+        loser_old_rank = self.ranking.loc[loser_id, "rank"]
+
+        if loser_old_rank is not None:
+            loser_old_score = self.ranking.loc[loser_id, "score"]
+            self.sorted_scores.remove(loser_old_score)
+
+        # We don't need the old score anymore so we can overwrite it.
+        self.ranking.loc[winner_id, "score"] = winner_score
+        self.ranking.loc[loser_id, "score"] = loser_score
+
+        # By default a player rank is None, it got a value only if the player
+        # played enough games.
+        winner_state["rank"] = None
+        loser_state["rank"] = None
+
+        winner_is_ranked = self.ranking.loc[winner_id, "n_games"] >= self.mingames
+        loser_is_ranked = self.ranking.loc[loser_id, "n_games"] >= self.mingames
+
+        # Both scores must be added before further processing to ensure the
+        # ranks are correct in all cases.
+        if winner_is_ranked:
+            self.sorted_scores.add(winner_score)
+
+        if loser_is_ranked:
+            self.sorted_scores.add(loser_score)
+
+        # Rank information is updated.
+        if winner_is_ranked:
+            winner_rank = self.sorted_scores.index(winner_score)
+            self.ranking.loc[winner_id, "rank"] = winner_rank
+            winner_state["rank"] = winner_rank
+
+        if loser_is_ranked:
+            loser_rank = self.sorted_scores.index(loser_score)
+            self.ranking.loc[loser_id, "rank"] = loser_rank
+            loser_state["rank"] = loser_rank
+
+        # Game IDs are incremented by one when the new game is appended
+        game_id = len(self.history)
+
+        # Add common information to the history dataframe
+        self.history = self.history.append(
+            dict(
+                timestamp=timestamp,
+                winner_id=winner_state["id"],
+                winner_rank=winner_state["rank"],
+                winner_score=winner_state["score"],
+                loser_id=loser_state["id"],
+                loser_rank=loser_state["rank"],
+                loser_score=loser_state["score"]
+            ),
+            ignore_index=True
+        )
+
+        # Now that all informations common to all rankings
+        # have been updated, we can let the subclass apply these new states
+        # without worrying about the side effects.
+        self.apply_new_states(game_id, winner_state, loser_state)
 
     async def update_leaderboard(self):
         """
@@ -108,86 +281,8 @@ class AbstractRanking:
                 raise KeyError(f"Leaderboard message of unkown type {T} "
                                f"in Ranking {self.name}.")
 
-    def compute_player_states(self, winner_id, loser_id, timestamp):
-        """
-        Compute the new states of the players involved in a game during which
-        the player with ID `winner_id` won against the player with ID
-        `loser_id`.
-
-        This function must not have any side effect, to make sure that
-        the generic `AbstractRanking.register_game` method works as expected.
-
-        Must return a dict with a field "score". This dict will later be passed
-        to the `AbstractRanking.apply_new_states` method in which additional
-        updates can be performed with side effects.
-
-        Must be implemented by subclasses.
-        """
-        raise NotImplementedError()
-
-    def register_game(self, winner_id=0, loser_id=0, timestamp=0, **kwargs):
-        # Ignore games that are outside considered period
-        if timestamp <= self.oldest_timestamp:
-            return None
-
-        if winner_id not in self.ranking.index:
-            self.add_new_player(winner_id)
-
-        if loser_id not in self.ranking.index:
-            self.add_new_player(loser_id)
-
-        winner_state, loser_state = self.compute_player_states(
-            winner_id,
-            loser_id)
-
-        winner_score = winner_state["score"]
-        loser_score = loser_state["score"]
-
-        self.ranking.loc[winner_id, "n_games"] += 1
-        self.ranking.loc[loser_id, "n_games"] += 1
-
-        winner_old_rank = self.ranking.loc[winner_id, "rank"]
-
-        if winner_old_rank is not None:
-            winner_old_score = self.ranking.loc[winner_id, "score"]
-            self.sorted_scores.remove(winner_old_score)
-
-        loser_old_rank = self.ranking.loc[loser_id, "rank"]
-
-        if loser_old_rank is not None:
-            loser_old_score = self.ranking.loc[loser_id, "score"]
-            self.sorted_scores.remove(loser_old_score)
-
-        self.ranking.loc[winner_id, "score"] = winner_score
-        self.ranking.loc[loser_id, "score"] = loser_score
-
-        winner_is_ranked = self.ranking.loc[winner_id, "n_games"] >= self.mingames
-        loser_is_ranked = self.ranking.loc[loser_id, "n_games"] >= self.mingames
-
-        winner_state["rank"] = None
-        loser_state["rank"] = None
-
-        # Both scores must be added first to ensure the ranks are correct
-        if winner_is_ranked:
-            self.sorted_scores.add(winner_score)
-
-        if loser_is_ranked:
-            self.sorted_scores.add(loser_score)
-
-        if winner_is_ranked:
-            winner_rank = self.sorted_scores.index(winner_score)
-            self.ranking.loc[winner_id, "rank"] = winner_rank
-            winner_state["rank"] = winner_rank
-
-        if loser_is_ranked:
-            loser_rank = self.sorted_scores.index(loser_score)
-            self.ranking.loc[loser_id, "rank"] = loser_rank
-            loser_state["rank"] = loser_rank
-
-        self.apply_new_states(timestamp, winner_state, loser_state)
-
-    def register_many(self, games):
-        raise NotImplementedError
+    # def register_many(self, games):
+    #     raise NotImplementedError
 
     # def get_kamlboard_stuff(self):
     #     # There are 3 potential scenarios:
--- src/ranking/trueskill_ranking.py
+++ src/ranking/trueskill_ranking.py
@@ -9,12 +9,37 @@ from utils import logger
 
 
 class TrueSkillRanking(AbstractRanking):
+    """
+    Ranking according to TrueSkill rating.
+
+    See trueskill.py documentation for the description of the keyword arguments.
+
+    Arguments
+    =========
+    name: str
+        The unique name of the ranking.
+
+    Keyword arguments
+    =================
+    mu: number
+    sigma: number
+    beta: number
+    tau: number
+
+    Addition keyword arguments are passed to the constructor of the super class.
+    """
     def __init__(self, name,
                  mu=25, sigma=25/3, beta=25/6, tau=25/300,
                  **kwargs):
 
-        logger.info(f"Init ranking {name}")
+        logger.info(f"Initialization of the TrueSkillRanking named '{name}' "
+                    f"with parameters:\n"
+                    f"  mu    = {mu}\n"
+                    f"  sigma = {sigma}\n"
+                    f"  beta  = {beta}\n"
+                    f"  tau   = {tau}")
 
+        # Initialize the TrueSkill environment
         self.ts_env = trueskill.TrueSkill(
             draw_probability=0.0,
             mu=mu,
@@ -22,7 +47,8 @@ class TrueSkillRanking(AbstractRanking):
             beta=beta,
             tau=tau)
 
-        self.history = pd.DataFrame(
+        # Initialize TrueSkill specific dataframes
+        history_dataframe = pd.DataFrame(
             columns=[
                 "timestamp",
                 "winner_id",
@@ -48,10 +74,14 @@ class TrueSkillRanking(AbstractRanking):
             ]
         )
 
-        super().__init__(name, ranking_dataframe=ranking_dataframe, **kwargs)
+        super().__init__(name,
+                         ranking_dataframe=ranking_dataframe,
+                         history_dataframe=history_dataframe,
+                         **kwargs)
 
     def add_new_player(self, player_id):
-        rating = self.ts_env.Rating()  # Rating with default values
+        # Init new players with a rating with envrionnement default values
+        rating = self.ts_env.Rating()
 
         player_data = pd.DataFrame(
             dict(
@@ -65,34 +95,16 @@ class TrueSkillRanking(AbstractRanking):
         )
         self.ranking = pd.concat([player_data, self.ranking])
 
-    def leaderboard(self, start, stop):
-        """Generate the string content of a leaderboard message."""
-        data = self.ranking[self.ranking["rank"].notna()].iloc[start:stop]
-        lines = []
+    def apply_new_states(self, game_id, winner_state, loser_state):
+        winner_rating = winner_state["rating"]
+        loser_rating = loser_state["rating"]
+        self.update_rating(winner_state["id"], winner_rating)
+        self.update_rating(loser_state["id"], loser_rating)
 
-        for player_id, row in data.iterrows():
-            wins = len(self.history[self.history["winner_id"] == player_id])
-            losses = len(self.history[self.history["loser_id"] == player_id])
-            lines.append(self.leaderboard_line.format(
-                rank=row["rank"],
-                score=row["score"],
-                sigma=row["sigma"],
-                leaderboard_name=self.players.loc[player_id, "display_name"],
-                wins=wins,
-                losses=losses
-            ))
-
-        new_content = "\n".join(lines)
-        return f"```\n{new_content}\n```"
-
-    def load_rating(self, player_id):
-        if player_id in self.ranking.index:
-            player_data = self.ranking.loc[player_id]
-            return self.ts_env.Rating(
-                mu=player_data["mu"],
-                sigma=player_data["sigma"])
-
-        return self.ts_env.Rating()
+        self.history.loc[game_id, "winner_mu"] = winner_rating.mu
+        self.history.loc[game_id, "winner_sigma"] = winner_rating.sigma
+        self.history.loc[game_id, "loser_mu"] = loser_rating.mu
+        self.history.loc[game_id, "loser_sigma"] = loser_rating.sigma
 
     def compute_player_states(self, winner_id, loser_id):
         winner_rating = self.load_rating(winner_id)
@@ -114,26 +126,43 @@ class TrueSkillRanking(AbstractRanking):
 
         return winner_state, loser_state
 
-    def apply_new_states(self, timestamp, winner_state, loser_state):
-        self.update_rating(winner_state["id"], winner_state["rating"])
-        self.update_rating(loser_state["id"], loser_state["rating"])
+    def leaderboard(self, start, stop):
+        # Mask selecting player in the correct rank range
+        mask = np.logical_and(self.ranking["rank"] >= start,
+                              self.ranking["rank"] < stop)
+        data = sorted(self.ranking[mask], lambda pid_row: pid_row[1]["rank"])
+        lines = []
 
-        self.history = self.history.append(
-            dict(
-                timestamp=timestamp,
-                winner_id=winner_state["id"],
-                winner_rank=winner_state["rank"],
-                winner_score=winner_state["score"],
-                winner_mu=winner_state["rating"].mu,
-                winner_sigma=winner_state["rating"].sigma,
-                loser_id=loser_state["id"],
-                loser_rank=loser_state["rank"],
-                loser_score=loser_state["score"],
-                loser_mu=loser_state["rating"].mu,
-                loser_sigma=loser_state["rating"].sigma
-            ),
-            ignore_index=True
-        )
+        for player_id, row in data:
+            wins = len(self.history[self.history["winner_id"] == player_id])
+            losses = len(self.history[self.history["loser_id"] == player_id])
+            lines.append(self.leaderboard_line.format(
+                rank=row["rank"],
+                score=row["score"],
+                sigma=row["sigma"],
+                leaderboard_name=self.bot.players.loc[player_id, "display_name"],
+                wins=wins,
+                losses=losses
+            ))
+
+        new_content = "\n".join(lines)
+        return f"```\n{new_content}\n```"
+
+    def load_rating(self, player_id):
+        """
+        Return the TrueSkill rating of a player given their player ID.
+
+        Arguments
+        =========
+        player_id: int
+        """
+        if player_id in self.ranking.index:
+            player_data = self.ranking.loc[player_id]
+            return self.ts_env.Rating(
+                mu=player_data["mu"],
+                sigma=player_data["sigma"])
+
+        return self.ts_env.Rating()
 
     def score(self, rating):
         return rating.mu - 3*rating.sigma
@@ -143,6 +172,7 @@ class TrueSkillRanking(AbstractRanking):
         self.ranking.loc[player_id, "sigma"] = new_rating.sigma
 
     def win_estimate(self, p1, p2):
+        # TODO Update this to the new architecture
         delta_mu = p1.mu - p2.mu
         sum_sigma2 = p1.sigma**2 + p2.sigma**2
         denom = sqrt(2 * self.ts_env.beta**2 + sum_sigma2)
